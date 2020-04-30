8a5438b136adb4d4cca547906cfd39862cd54258
91900 <91900@DESKTOP-VC6QHVC>
2020-04-30 23:46:09 +0530

    This is my test git again...

---
+++ SeleniumJenkins/src/test/java/stepDefinations/TesingGitCommit.java
@@ -0,0 +1,11 @@
+package stepDefinations;
+
+public class TesingGitCommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testing Git commit ....Baby !!!");
+	}
+
+}
